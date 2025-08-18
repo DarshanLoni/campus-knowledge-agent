868bcc44d6f3d22e7395fd85bd95c3baebe5553f
Darshan Loni <132261307+DarshanLoni@users.noreply.github.com>
2025-08-18 17:14:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run FastAPI
+python -m uvicorn app.main:app --host 0.0.0.0 --port 10000
